702735134339df5711926e1054d858e47185dd72
CHANGEL <107169027+CHANGEL1004@users.noreply.github.com>
2023-10-27 01:31:17 +0900

    [feat] : fragment 구현

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/HomeFragment.kt
@@ -0,0 +1,47 @@
+package org.sopt.dosopttemplate.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.sopt.dosopttemplate.data.DummyFriendsData
+import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
+import org.sopt.dosopttemplate.presentation.adapter.FriendsSealedAdapter
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = requireNotNull(_binding) { "바인딩 error" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // RecyclerView에 어댑터 설정하고 더미 데이터 추가
+        setupRecyclerView()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // 뷰 바인딩 해제
+        _binding = null
+    }
+
+    private fun setupRecyclerView() {
+        val friendsSealedAdapter = FriendsSealedAdapter(requireContext())
+        binding.rvFriends.adapter = friendsSealedAdapter
+        friendsSealedAdapter.setFriendsData(ArrayList(DummyFriendsData.dummyFriendList))
+    }
+}
+
+
