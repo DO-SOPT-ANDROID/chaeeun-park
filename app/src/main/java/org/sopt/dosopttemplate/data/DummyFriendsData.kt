292d73c8ab5c68786e8d07ea886c7c3568f7a21e
CHANGEL <107169027+CHANGEL1004@users.noreply.github.com>
2023-11-03 23:53:09 +0900

    [fix]: friends 데이터 sealed 클래스 변경

--- app/src/main/java/org/sopt/dosopttemplate/data/DummyFriendsData.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/DummyFriendsData.kt
@@ -8,7 +8,7 @@ object DummyFriendsData {
             profileImage = R.drawable.angel,
             name = "박채은",
         ),
-        FriendsSealed.FriendsNormal(
+        FriendsSealed.FriendsBirthday(
             profileImage = R.drawable.img_default__profile,
             name = "SOPT OB 사과 ",
             description = "나는 빨간색",
@@ -18,10 +18,11 @@ object DummyFriendsData {
             name = "SOPT OB 바나나",
             description = "나는 노란색",
         ),
-        FriendsSealed.FriendsNormal(
+        FriendsSealed.FriendsMelon(
             profileImage = R.drawable.img_default__profile,
             name = "SOPT YB 포도",
             description = "나는 보라색",
+            musicTitle = "숲 - 최유리"
         ),
         FriendsSealed.FriendsNormal(
             profileImage = R.drawable.img_default__profile,
@@ -33,10 +34,11 @@ object DummyFriendsData {
             name = "SOPT YB 샤인머스캣",
             description = "나는 망고맛 포도",
         ),
-        FriendsSealed.FriendsNormal(
+        FriendsSealed.FriendsMelon(
             profileImage = R.drawable.img_default__profile,
             name = "SOPT YB 복숭아",
             description = "나는 딱복",
+            musicTitle = "좋은날 - 아이유"
         ),
         FriendsSealed.FriendsNormal(
             profileImage = R.drawable.img_default__profile,
@@ -53,10 +55,11 @@ object DummyFriendsData {
             name = "SOPT YB 파인애플",
             description = null,
         ),
-        FriendsSealed.FriendsNormal(
+        FriendsSealed.FriendsMelon(
             profileImage = R.drawable.img_default__profile,
             name = "SOPT YB 토마토",
             description = "나는 멋쟁이",
+            musicTitle = "중독 - 엑소"
         ),
         FriendsSealed.FriendsNormal(
             profileImage = R.drawable.img_default__profile,
