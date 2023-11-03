2b3fa6d9032ddf3e35d942be73ae7eaf2c1d01eb
CHANGEL <107169027+CHANGEL1004@users.noreply.github.com>
2023-11-03 23:54:31 +0900

    [ADD]: 멜론 생일 ViewHolder 추가

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/adapter/FriendsMelonViewHolder.kt
@@ -0,0 +1,19 @@
+package org.sopt.dosopttemplate.presentation.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.dosopttemplate.data.FriendsSealed
+import org.sopt.dosopttemplate.databinding.ItemFriendsMelonBinding
+
+class FriendsMelonViewHolder(private var binding: ItemFriendsMelonBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+        fun onBindView(friendsMelonData: FriendsSealed.FriendsMelon) {
+            binding.run {
+                ivFriendsMelonProfile.setImageResource(friendsMelonData.profileImage)
+                ivFriendsMelonProfile.clipToOutline = true
+                tvFriendsMelonName.text = friendsMelonData.name
+                tvFriendsMelonContent.text = friendsMelonData.description
+                tvFriendsMelonMusic.text = friendsMelonData.musicTitle
+            }
+        }
+    }
